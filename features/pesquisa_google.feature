4bf079c034781c68a8e6414ad43db72d6f380582
Matheus Geambastiane <mgeambastiane@gmail.com>
2023-09-26 18:56:27 -0300

    Atividade de pesquisa

---
+++ features/pesquisa_google.feature
@@ -0,0 +1,11 @@
+#language: pt
+
+Funcionalidade: Pesquisa no google
+
+
+    Cenário: Pesquisar o Instituto Joga Junto
+
+    Dado que o usuario esteja na pagina do google
+    Quando ele clicar e pesquisar o Instituto
+    Então O titulo da pagina sera sobre o resultado da pesquisa: instituto joga junto - Pesquisa Google
+
---
+++ features/steps/pesquisa_ijj_step.py
@@ -0,0 +1,34 @@
+from behave import given, when, then
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from time import sleep
+
+
+@given("que o usuario esteja na pagina do google")
+def go_to_page(context):
+    context.browser = Firefox()
+    context.browser.get("https://google.com")
+
+@when("ele clicar e pesquisar o Instituto")
+def search_for_instituitio(context):
+    search_bar = context.browser.find_element(By.NAME, "q")
+    search_bar.click()
+    search_bar.send_keys("instituto joga junto")
+    search_bar.send_keys(Keys.ENTER)
+
+@then("O titulo da pagina sera sobre o resultado da pesquisa: instituto joga junto - Pesquisa Google")
+def verify_title(context):
+    expect_title = "instituto joga junto - Pesquisa Google"
+    title = context.browser.title
+    import ipdb;ipdb.sset_trace()
+    print(title)
+    sleep(8)
+
+    title = context.browser.title
+
+
+
+
+    assert expect_title in title
